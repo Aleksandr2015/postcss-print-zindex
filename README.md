19b54ec55fdf8c78265807794d675713942757ee
rosivanov <ros.g6w@gmail.com>
2016-12-29 00:41:20 +0200

    index.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PostCSS Print Zindex [![Build Status][ci-img]][ci]
 
-[PostCSS] plugin prints z-index values into terminal..
+[PostCSS] plugin just prints z-index values into terminal.
 
 [PostCSS]: https://github.com/postcss/postcss
 [ci-img]:  https://travis-ci.org/rosivanov/postcss-print-zindex.svg
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var postcss = require('postcss');
+var postcss = require('postcss'),
+    Table = require('cli-table'),
+    chalk = require('chalk');
+
+// Table headers
+const table = new Table({
+    head: [chalk.green('Z-index Value'), chalk.green('Selector'), chalk.green('From')]
+});
 
 module.exports = postcss.plugin('postcss-print-zindex', function (opts) {
     opts = opts || {};
@@ -8,6 +15,17 @@ module.exports = postcss.plugin('postcss-print-zindex', function (opts) {
     return function (root, result) {
 
         // Transform CSS AST here
+        root.walkDecls('z-index', decl => {
+            const zindex_val = decl.value > 5 ? chalk.bgRed(decl.value) : chalk.yellow(decl.value);
+            table.push([
+                zindex_val,
+                chalk.magenta(decl.parent.selector),
+                `${chalk.gray(decl.source.input.from)}\n\line: ${decl.source.start.line}`
+            ]);
+        });
+
+        // Print table
+        console.log(table.toString());
 
     };
 });
